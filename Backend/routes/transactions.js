c3f5eb64afe516c716891fcbe66b1d3c0d44907b
Zubeyr19 <zuabd22@student.sdu.dk>
2024-03-19 11:34:44 +0100

    removed a line that made npm crash

--- Backend/routes/transactions.js
+++ Backend/routes/transactions.js
@@ -1,4 +1,3 @@
-const { addExpense, getExpense, deleteExpense } = require('../controllers/expense');
 const { addIncome, getIncomes, deleteIncome } = require('../controllers/income');
 
 const router = require('express').Router();
